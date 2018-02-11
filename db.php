a097c7c7d8f7f59a3bd7b4a563b269da29b6857d
rifmeister <chinsanity16@gmail.com>
2018-02-11 19:02:02 +0000

    connexion a la db

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $password = '';
 $hostname = 'localhost';
 
 try{
-    $bdd = new PDO('mysql:host='.$hostname.';dbname=monapp;charset=utf8', $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host='.$hostname.';dbname=parc_auto;charset=utf8', $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exception $e)
 {
